cfb67da4f1c6d20f17f4ced0f3b6d30ff4a6a437
premk <premk@SHADOW>
2023-06-07 23:59:20 +0530

    Add project files.

---
+++ dotnet-date-time/Program.cs
@@ -0,0 +1,6 @@
+﻿//just date and time
+
+DateTime today = DateTime.Now;
+Console.WriteLine(today);
+Console.WriteLine($"printing in DD-MM-YYYY : {today.ToString("dd-MM-yyyy")}");
+Console.WriteLine($"printing AM or PM : {today.ToString("tt")}");
